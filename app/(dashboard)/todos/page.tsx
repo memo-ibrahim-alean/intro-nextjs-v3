8c22131856211888fc52c88ff6c1f27644eeea77
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-22 20:06:18 +0300

    Fetching Data with Prisma

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,6 +1,19 @@
-function TodosPage() {
+import TodoList from '@/components/TodoList'
+import db from '@/utils/db'
+
+const getData = async () => {
+  // await new Promise((resolve, reject) => setTimeout(() => reject(), 2000))
+  const todos = await db.todo.findMany({})
+  return todos
+}
+
+const TodosPage = async () => {
+  const todos = await getData()
+  console.log(todos);
   return (
-    <div>Todos Page</div>
+    <div>
+      <TodoList todos={todos} />
+    </div>
   )
 }
 
---
+++ components/TodoList.tsx
@@ -0,0 +1,15 @@
+import Todo from "./Todo"
+
+const TodoList = ({ todos }) => {
+  return (
+    <div>
+      {
+        todos.map( (todo) => (
+          <Todo todo={todo} key={todo.id} />
+        ))
+      }
+    </div>
+  )
+}
+
+export default TodoList
