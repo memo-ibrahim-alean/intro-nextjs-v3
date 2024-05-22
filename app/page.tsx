a997ba22177294522cec8d00b28cf64911bc421e
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-22 12:15:15 +0300

    Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div>Home</div>
-  );
+const getData = async () => {
+  await new Promise((resolve) => setTimeout(() => resolve(0), 2000));
+  return {data: [1, 2, 3]}
 }
+
+const Home = async () => {
+  const { data } = await getData();
+  console.log(data);
+  return <div>Home</div>;
+}
+
+export default Home;
