89c5fa0b02c3449db3e782f5d3b17c85c5339d48
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-22 11:15:27 +0300

    Layouts & Templates

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,11 @@
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <>
+      <div>Dashboard Layout</div>
+      <div>{ children }</div>
+    </>
+  )
+}
+
+export default DashboardLayout
