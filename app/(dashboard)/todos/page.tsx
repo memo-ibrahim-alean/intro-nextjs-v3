0449eb9d2bec626390f4cf1689d612673bed3c4d
Mohamed Ibrahim <109217422+memo-ibrahim-alean@users.noreply.github.com>
2024-05-23 12:30:18 +0300

    Loading & Error

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -2,7 +2,7 @@ import TodoList from '@/components/TodoList'
 import db from '@/utils/db'
 
 const getData = async () => {
-  // await new Promise((resolve, reject) => setTimeout(() => reject(), 2000))
+  await new Promise((resolve, reject) => setTimeout(() => reject(), 2000))
   const todos = await db.todo.findMany({})
   return todos
 }
